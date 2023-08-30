211b23549c4274ae0fa624e31ff54953458c2641
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-08-30 11:07:49 +0200

    commit

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 import streamlit as st
 import pandas as pd
 import time
+import plotly.express as px
 from modules.rand_data import generate_random_coordinates_list
 from modules.telegramm_update import get_update
 
-# Optional change Mapbox map to plotly Map. https://plotly.com/python/scattermapbox/
 
-df_dummy = get_update()
-output_df = preprocess(df_dummy)
-
-
+# Define a CSS class for custom styling
 def main():
     lat_list, lon_list = generate_random_coordinates_list()
     data = {"Location": ["Berlin"] * 100, "LAT": lat_list, "LON": lon_list}
-    berlin_df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
     datetimenow = time.strftime("%H:%M:%S")
     st.title(f"BVG Controllers Berlin - {datetimenow}")
-    st.map(berlin_df, zoom=10, color="#ffaa0088", size=50)
-    # Add a refresh button
-    st.table(berlin_df)
+
+    px.set_mapbox_access_token(open("access.mapbox_token").read())
+    fig = px.scatter_mapbox(
+        df,
+        lat="LAT",
+        lon="LON",
+        color_continuous_scale=px.colors.cyclical.IceFire,
+        size_max=15,
+        zoom=10,
+    )
+
+    st.plotly_chart(fig, use_container_width=False, width=800)
+    st.table(df.style.set_table_attributes('class="custom-table"'))
+
     output = st.empty()
 
     while True:

b1b276f9b2d232f049585cbc6d51384bc3fbfab5
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-08-30 08:51:40 +0200

    Infrastructure_streamlit_only

--- app.py
+++ app.py
@@ -3,32 +3,11 @@ import streamlit as st
 import pandas as pd
 import random
 import time
-
-
-@task
-def generate_random_coordinates():
-    min_lat, max_lat = 52.392166, 52.639004
-    min_lon, max_lon = 13.215260, 13.770269
-    random_lat = random.uniform(min_lat, max_lat)
-    random_lon = random.uniform(min_lon, max_lon)
-    return random_lat, random_lon
-
-
-@task
-def generate_random_coordinates_list(num_samples=100):
-    lat_list = []
-    lon_list = []
-    for x in range(num_samples):
-        random_lat, random_lon = generate_random_coordinates()
-        lat_list.append(random_lat)
-        lon_list.append(random_lon)
-    return lat_list, lon_list
-
+from modules.rand_data import generate_random_coordinates_list
 
 # Optional change Mapbox map to plotly Map. https://plotly.com/python/scattermapbox/
 
 
-@flow
 def main():
     lat_list, lon_list = generate_random_coordinates_list()
     data = {"Location": ["Berlin"] * 100, "LAT": lat_list, "LON": lon_list}

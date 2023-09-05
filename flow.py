cc796d798a28c8200c3f3f4f671e4373ca3ef6a2
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-09-05 15:18:55 +0200

    update

--- flow.py
+++ flow.py
@@ -6,7 +6,13 @@ from ticket_control.big_query_download_processed import *
 from ticket_control.big_query_upload_processed import *
 from ticket_control.telegramm_update_prod import *
 from prefect_gcp import GcpCredentials
+from prefect.filesystems import GitHub
+from prefect_github import GitHubCredentials
+from prefect_github.repository import GitHubRepository
 
+github_repository_block = GitHubRepository.load("github-repo2")
+github_credentials_block = GitHubCredentials.load("github")
+github_block = GitHub.load("github-repo")
 gcp_credentials_block = GcpCredentials.load("prefect")
 
 

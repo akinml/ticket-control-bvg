aeb93c2b5e92eb03b651cf72e5ac4e0b59f91d21
Anna Kravets <aniekrav@gmail.com>
2023-09-07 09:48:59 +0200

    use st secrets instead

--- ticket_control/params.py
+++ ticket_control/params.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-import os
+import streamlit as st
 
 path_main = Path(__file__).parent.parent
 print(Path(__file__))
-path_to_data = os.environ.get('PATH_TO_DATA', None)
+path_to_data = st.secrets.get('PATH_TO_DATA', None)
 if path_to_data is None:
     path_to_data = path_main / "data/"

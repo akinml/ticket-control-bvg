a325b4e6eab861dae09b133f6893469889491ccf
Anna Kravets <aniekrav@gmail.com>
2023-09-07 09:40:31 +0200

    add env variable to params

--- ticket_control/params.py
+++ ticket_control/params.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import os
 
 path_main = Path(__file__).parent.parent
 print(Path(__file__))
-path_to_data = path_main / "data/"
+path_to_data = os.environ.get('PATH_TO_DATA', None)
+if path_to_data is None:
+    path_to_data = path_main / "data/"

799fed9815cb00687d10e2f9a3dd2f19376f210e
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-09-07 06:30:09 +0200

    commit

--- ticket_control/big_query_download_processed.py
+++ ticket_control/big_query_download_processed.py
@@ -1,13 +1,14 @@
 from datetime import timedelta
 from google.cloud import bigquery
 import pandas as pd
+from prefect_gcp import GcpCredentials
+
+gcp_credentials_block = GcpCredentials.load("gcp")
 
 
 def download_big_query_processed():
     print("\033[1;32m 👷Downloading from BigQuery Started! 👷\n")
-    client = bigquery.Client.from_service_account_json(
-        "bvg-controller-a5a989d34b1d.json"
-    )
+    client = bigquery.Client.from_service_account_info(gcp_credentials_block)
     query = """
     SELECT *
     FROM `bvg-controller.bvg_test.processed`

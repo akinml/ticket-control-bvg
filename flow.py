eee59ab8089fef7b412ae0c7e7ad255950a62e3d
Christoph Bieritz <christoph.bieritz@hotmail.de>
2023-09-07 09:51:26 +0200

    commit

--- flow.py
+++ flow.py
@@ -1,4 +1,4 @@
-from prefect import flow
+from prefect import flow, task
 from ticket_control.pipeline import *
 from ticket_control.big_query_download_raw import *
 from ticket_control.big_query_upload_raw import *
@@ -9,12 +9,18 @@ from prefect.task_runners import SequentialTaskRunner
 
 
 @flow(name="pipeline", task_runner=SequentialTaskRunner())
-def update_flow():
-    df_tele_raw = get_update()
+@task(name="update")
+def get_update_flow():
+    return get_update()
+
+
+@task(name="flow")
+def update_flow(df_tele_raw):
     df_tele_processed = pipeline(df_tele_raw)
     upload_big_query_raw(df_tele_raw)
     upload_big_query_processed(df_tele_processed)
 
 
 if __name__ == "__main__":
-    update_flow()
+    raw = get_update_flow()
+    update_flow(raw)
